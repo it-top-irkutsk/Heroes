0593204151cbe030f10ca86a25402403ebd9134b
Starinin Andrey <starininandrey@gmail.com>
2022-05-18 16:30:51 +0300

    Add game

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <string>
+
+#include "Hero.h"
+#include "Mage.h"
+#include "Barbarian.h"
+
+#include "CLI.h"
+
+using namespace std;
 
 int main() {
 
+    Hero* hero;
+    Hero* enemy;
+
+    CLI::welcome();
+    char selectType;
+    cout << "Select character type:" << endl;
+    cout << "1 -> Mage" << endl;
+    cout << "2 -> Barbarian" << endl;
+    cin >> selectType;
+
+    string name;
+    cout << "Enter character name: ";
+    cin >> name;
+
+    switch (selectType) {
+        case '1':
+            hero = new Mage(name);
+            enemy = new Barbarian("Enemy");
+            break;
+        case '2':
+            hero = new Barbarian(name);
+            enemy = new Mage("Enemy");
+            break;
+    }
+
+    CLI::printHero(hero);
+    CLI::printHero(enemy);
+
+    hero->attack(enemy);
+    enemy->attack(hero);
+
+    CLI::printHero(hero);
+    CLI::printHero(enemy);
 
     return 0;
 }
